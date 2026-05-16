fbf5cd6294c27a9dc5cc3a694c88215707b8f717
om kumar singh <omsingh.ethdev@gmail.com>
2026-05-16 12:38:06 +0530

    v2: Add user and group creation script with validation

--- setup.sh
+++ setup.sh
@@ -1,30 +1,56 @@
 #!/usr/bin/env bash
 
 echo "Starting setup....."
-read -p "Enter the users name to create:" -a  users
-read -p "Enter the groups name to create:" -a groups
+echo "-------------------------------------------------"
+read -p "Enter  users :" -a  users
+read -p "Enter groups :" -a groups
+	
+if [[ "${#users[@]}" -eq 0 && "${#groups[@]}" -eq 0 ]]; then
+		echo "Both user and group is empty.Exiting..."
+		exit 1
+fi
 
-echo "Creating the group..."
-
+	
+if [[ "${#groups[@]}" -eq 0 ]]; then
+	echo "No group entered.Skipping group creation..."
+else
+echo "Creating group..."
 for group in "${groups[@]}"; do
-	if  getent group "${group}" ; then
-		echo "Group "${group}" already exist."
+	if  getent group "${group}" > /dev/null ; then
+		echo "Group ${group} already exist."
 	else
-		echo "Creating "${group}". "
-		sudo groupadd "${group}"
+		echo "Creating ${group}. "
+		
+		if sudo groupadd "${group}"; then
+			echo "${group} created."
+		else
+			echo "Error! in creating group ${group}"
+		fi
+
  	fi
 done
+fi
 
+echo "------------------------------------------------"
+
+if [[ "${#users[@]}" -eq 0 ]]; then
+	echo "No user entered.Skipping user creation..."
+else
 echo "Creating the user..."
 
 for user in "${users[@]}"; do
-	if getent passwd "${user}" ; then
-		echo "User "${user}" alreay exits."
+	if getent passwd "${user}" > /dev/null; then
+		echo "User ${user} already exists."
 	else
-		echo "Creating "${user}"."
-		sudo useradd -m "${user}"
+		echo "Creating ${user}."
+		if sudo useradd -m "${user}"; then	
+			echo "${user} is created."
+		else
+			echo "ERROR! in creating user ${user}."
+		fi
 	fi
 done
-echo "All users and groups are created"
+fi
+echo "------------------------------------------------"
 
 
